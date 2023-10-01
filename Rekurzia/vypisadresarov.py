fb716ad78d697035179eee74c679fe469bf539b2
kolokoc0 <kristiansvrlo@gmail.com>
2023-10-01 20:20:03 +0200

    cmon

--- Rekurzia/vypisadresarov.py
+++ Rekurzia/vypisadresarov.py
@@ -4,7 +4,7 @@ def Folders(path,depth=0):
     for item in os.listdir(path):
         if os.path.isdir(path+"\\"+item) and item[0] not in ".$":
             if len(os.listdir(path+"\\"+item))!=0: #vrati aj subory ktore su v danom adresari treba len adresare nie subory
+                print('-'*depth,item)
                 Folders(path+"\\"+item,depth+1)
-            else:
-                print("-"*depth,item)
-Folders("D:\\škola\\")
+        
+Folders("D:\\Skola")
