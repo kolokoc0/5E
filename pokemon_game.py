5a16b470070c058e7c6ce346e4c9eba289b101f7
kolokoc0 <kristiansvrlo@gmail.com>
2023-10-02 20:29:02 +0200

    finally finalproof?

--- pokemon_game.py
+++ pokemon_game.py
@@ -44,7 +44,6 @@ with open ('effectiveness_new.json') as file:
 #     print(cumTypeAdv)
 
 def calculator(pokemons1,pokemons2):
-    resultduel = 1
     duels = []
     duo_list = []
     cumTypeAdv = 0
@@ -56,31 +55,37 @@ def calculator(pokemons1,pokemons2):
             for type_attack in attack:
                 for type_defense in defense:
                     duo_list.append(effects[type_attack][type_defense])
+
             if len(attack) >= 2 and len(defense) ==1:
                 cumTypeAdv += max(duo_list)
+
             elif len(attack) >= 2 and len(defense) >= 2:
-                for i in range(0,len(duo_list),len(attack)):
-                    for num in duo_list[i:i+len(attack):]:
-                        resultduel = resultduel * num
-                    duels.append(resultduel)
-                    resultduel = 1   
+                for i in range(0,len(duo_list),len(defense)):
+                    for num in duo_list[i:i+len(defense):]:
+                        result = result * num
+                    duels.append(result)
+                    result = 1   
                 cumTypeAdv += max(duels)
                 duels = [ ]
+
             elif len(attack) == 1 and len(defense) ==1:
                 cumTypeAdv += max(duo_list)
+
             elif len(attack) == 1 and len(defense) >=2:
                 for i in duo_list:
                     result = result *i
                 cumTypeAdv += result
+
             result = 1
             duo_list = [ ]
+
     return(cumTypeAdv)
 
 
 def game(number1,number2,listpok):
     listpok = listpok.split(",")
     pokemons1 = listpok[0:number1:]
-    pokemons2 = listpok[number1:number2+number2:]
+    pokemons2 = listpok[number1:number1+number2:]
     player1_val = calculator(pokemons1,pokemons2)
     player2_val = calculator(pokemons2,pokemons1)
     if player1_val >player2_val:
@@ -88,6 +93,6 @@ def game(number1,number2,listpok):
     elif player1_val==player2_val:
         return "{:.1f}".format(player1_val),"{:.1f}".format(player2_val),"BALANCE"
     elif player1_val<player2_val:
-        return "{:.1f}".format(player1_val),"{:.1f}".format(player2_val),"BALANCE"
+        return "{:.1f}".format(player1_val),"{:.1f}".format(player2_val),"FOE"
 
 print(game(4,4,"Psychic Dark,Fire,Ghost Ice,Fairy Electric,Normal Steel,Ghost,Poison Fire,Dark Bug"))
